73ca2eb92a2b666f4032ae4405f7f32e5a080b48
Fabio Farzat <fabiofarzat@gmail.com>
2013-07-20 11:17:29 -0300

    Setup das classes de Entidade

---
+++ Db4oEntidades/IEntidade.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Db4oEntidades
+{
+    public class IEntidade<T>
+    {
+        T Id { get; set; }
+    }
+}
